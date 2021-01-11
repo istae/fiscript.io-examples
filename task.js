db308a557a95fb0a9b5a1a27c859147be7274bdd
Esad Akar <eakar2012@fau.edu>
2021-01-11 11:15:07 -0500

    Create task.js

---
+++ task.js
@@ -0,0 +1,46 @@
+const OPEN = 0, HIGH = 1, LOW = 2, CLOSE = 3, BASEVOLUME = 4, QUOTEVOLUME = 5, CANDLEVALUES = 6;
+
+export function task({ symbols, candles }) {
+
+    const list = symbols().filter(s => s.endsWith('USDT'))
+
+    const data = list.map(s => {
+        // past 100 days in minutes
+        const sticks = candles(s, 100 * 24 * 60)
+        if (sticks.length == 0) return null
+        const avg = EMA(sticks, 50)
+        const price = sticks[sticks.length - CANDLEVALUES + CLOSE]
+        return {
+            symbol: s,
+            avg,
+            price,
+            change: ((avg - price) / avg) * 100
+        }
+    })
+
+    data.sort((a, b) => b.change - a.change).slice(0, 10).forEach(d => {
+        console.log(`${d.symbol} down ${d.change.toFixed(2)}% at ${d.price.toFixed(3)} avg ${d.avg.toFixed(3)}`);
+    })
+
+    data.sort((a, b) => a.change - b.change).slice(0, 10).forEach(d => {
+        console.log(`${d.symbol} up ${Math.abs(d.change).toFixed(2)}% at ${d.price.toFixed(3)} avg ${d.avg.toFixed(3)}`);
+    })
+}
+
+
+function EMA(data, days) {
+
+    const impact = 2.7182818 / (24 * 60 * days)
+
+    let average = data[CLOSE];
+
+    const count = data.length / CANDLEVALUES;
+
+    for (let i = 0; i < count; i++) {
+        const offset = i * CANDLEVALUES;
+        const price = (data[offset + HIGH] + data[offset + LOW] + data[offset + CLOSE]) / 3;
+        average = average + ((price - average) * impact)
+    }
+
+    return average
+}
