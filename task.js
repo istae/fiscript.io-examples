d4475602d64821461851dc9f0fa63ba54f7e3b4e
Esad Akar <eakar2012@fau.edu>
2021-02-10 22:28:38 -0500

    Update task.js

--- task.js
+++ task.js
@@ -2,40 +2,44 @@ const OPEN = 0, HIGH = 1, LOW = 2, CLOSE = 3, BASEVOLUME = 4, QUOTEVOLUME = 5, C
 
 export function task({ symbols, candles }) {
 
-    const list = symbols().filter(s => s.endsWith('USDT'))
-    
-    console.log(list)
+    //const list = symbols().filter(s => s.endsWith('USDT'))
+    const list = ["BTC", "BNB", "ETH", "LINK", "XLM", "ALGO", "ATOM", "BAND", "BAT",
+				  "ADA", "VET", "UNI", "OMG", "SUSHI", "XMR"].map(x => x + "USDT")
 
     const data = list.map(s => {
-        // past 75 days in minutes
-        const sticks = candles(s, 75 * 24 * 60)
-        const avg = EMA(sticks, 50)
+	// 300 days of data
+        const sticks = candles(s, 300 * 24 * 60)
+	// 90 day moving average
+        const avg = EMA(sticks, 90)
         const price = sticks[sticks.length - CANDLEVALUES + CLOSE]
         return {
             symbol: s,
             avg,
             price,
-            change: ((avg - price) / avg) * 100
+            change: -((avg - price) / avg) * 100
         }
     })
 	
     console.log("--------------------------------------")
 
-    data.sort((a, b) => b.change - a.change).slice(0, 10).forEach(d => {
+    data.sort((a, b) => a.change - b.change).forEach(d => {
+		if (d.change > 0) return;
         console.log(`${d.symbol} down ${d.change.toFixed(2)}% at ${d.price.toFixed(3)} avg ${d.avg.toFixed(3)}`);
     })
     
     console.log("--------------------------------------")
 
-    data.sort((a, b) => a.change - b.change).slice(0, 10).forEach(d => {
-        console.log(`${d.symbol} up ${Math.abs(d.change).toFixed(2)}% at ${d.price.toFixed(3)} avg ${d.avg.toFixed(3)}`);
+    data.sort((a, b) => b.change - a.change).forEach(d => {
+		if (d.change < 0) return;
+        console.log(`${d.symbol} up ${d.change.toFixed(2)}% at ${d.price.toFixed(3)} avg ${d.avg.toFixed(3)}`);
     })
 }
 
 
 function EMA(data, days) {
-
-    const impact = 2.7182818 / (24 * 60 * days)
+	
+	// 2.7182818
+    const impact = 2 / (24 * 60 * days)
 
     let average = data[CLOSE];
 
